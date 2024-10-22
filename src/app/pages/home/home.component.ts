908d63e9462e5dd0f72a5a5ec61e8fac88dc28eb
Joao <j_donizetti@outlook.com>
2024-10-21 23:24:54 -0300

    finalizado o component menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
