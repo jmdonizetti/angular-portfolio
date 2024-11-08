fdfebf277072c7b072e3e18977a811d5d89f4a61
Joao <j_donizetti@outlook.com>
2024-11-08 12:56:47 -0300

    Feito o Id do sobre para quando o usuario clicar ele ir para a contentPage, foi preciso colocar o constructor com uma rota privada usando o ActivatedRoute e criar um metodo no ngOnInit()

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-content',
@@ -8,8 +9,18 @@ import { RouterLink } from '@angular/router';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
   photoCover:string = "https://i0.wp.com/fiosdenylon.com.br/wp-content/uploads/2017/09/super-mario.jpg?resize=1010%2C1515"
   contentTitle:string = "About"
   contentDescription:string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum corrupti sit quia illum consectetur tempora vel voluptatum voluptatibus consequuntur veniam dolorum, rem voluptates, itaque eligendi, at laborum a deleniti sunt."
+
+  constructor(
+    private route:ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+   this.route.paramMap.subscribe( value =>
+    console.log(value.get("id"))
+   )
+  }
 }
