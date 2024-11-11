436deda3930e772403cf8df3d023908e8a742e69
Joao <j_donizetti@outlook.com>
2024-11-11 20:02:30 -0300

    deixando a pagina de conteudo dinamica, pegando os dados do meu dataFake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":1,
-    "photo":"aaa",
-    "title": "Sobre",
-    "description": "ola"
+    "title": "João Marcos Donizetti Rodrigues",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, delectus dolores nemo ipsa maxime ea excepturi ducimus repellendus doloribus et qui dolorem consequatur optio corrupti. Soluta pariatur nulla sequi totam?",
+    "photoCover": "https://avatars.githubusercontent.com/u/112431459?v=4"
   },
   {
     "id":2,
-    "photo":"aaa",
-    "title": "Sobre",
-    "description": "ola"
+    "title": "Escreva o titulo aqui",
+    "description": "Escreva a discrição aqui",
+    "photoCover": "coloque o caminho ou o link da imagem aqui "
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { dataFake } from "../../data/dataFake";
 
 
 @Component({
@@ -10,9 +11,10 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './content.component.css'
 })
 export class ContentComponent implements OnInit{
-  photoCover:string = "https://i0.wp.com/fiosdenylon.com.br/wp-content/uploads/2017/09/super-mario.jpg?resize=1010%2C1515"
-  contentTitle:string = "About"
-  contentDescription:string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum corrupti sit quia illum consectetur tempora vel voluptatum voluptatibus consequuntur veniam dolorum, rem voluptates, itaque eligendi, at laborum a deleniti sunt."
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+  private id:string | null = "0"
 
   constructor(
     private route:ActivatedRoute
@@ -20,7 +22,17 @@ export class ContentComponent implements OnInit{
 
   ngOnInit(): void {
    this.route.paramMap.subscribe( value =>
-    console.log(value.get("id"))
+    this.id = (value.get("id"))
    )
+
+   this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id.toString() == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 }
