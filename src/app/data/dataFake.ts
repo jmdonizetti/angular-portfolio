83d870d09db4808adefb21447cef257b33915180
Joao <j_donizetti@outlook.com>
2024-12-02 17:14:04 -0300

    content page estilizada e hover adicionado na imagem de todos os cards

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,8 @@
 export const dataFake = [
   {
     "id":1,
-    "title": "João Marcos Donizetti Rodrigues",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, delectus dolores nemo ipsa maxime ea excepturi ducimus repellendus doloribus et qui dolorem consequatur optio corrupti. Soluta pariatur nulla sequi totam?",
+    "title": "JOÃO MARCOS DONIZETTI RODRIGUES",
+    "description": "Olá, me chamo João Marcos, tenho 26 anos, sou casado e tenho um filho de 4 anos chamado Apollo. Sou apaixonado por tecnologia, desde quando eu era pequeno e entrava na internet tinha curiosidade para saber como que um site era feito, pesquisei e descobri a área de programação, iniciei minha faculdade no curso de Análise e desenvolvimento de sistemas, comecei a estudar HTML5, CSS3 e JavaScript, conforme o tempo foi passando me aprofundei no universo da programação, fui selecionado para a turma da ADA/Santander coders 2023 front-end Angular. Com esse programa aprofundei e aprendi ainda mais sobre HTML, CSS, JavaScript e aprendi também o que é TypeScript e Angular. Hoje venho realizando alguns projetos para melhorar e aprender ainda mais sobre desenvolvimento web com Angular, estou a procura da minha primeira oportunidade no mercado de trabalho para finalmente iniciar a minha carreira como desenvolvedor.",
     "photoCover": "https://avatars.githubusercontent.com/u/112431459?v=4"
   },
   {
